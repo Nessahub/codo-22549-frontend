bfaad3ca940373f019928089f1ddf460ebd584ca
Sabrina <marchiori.sabrina@hotmail.com>
2022-09-15 19:59:28 -0300

    Clase13

---
+++ Clase13/JS/operaciones.js
@@ -0,0 +1,20 @@
+
+ /* comentario en JS */ // comentario en JS 
+ function saludar() {
+    alert('hola mundo dentro de saludar');
+} 
+/* OPERADORES */
+/* + - * / */
+// variable: cadena: "hoal soy sabrina"
+//           numeros: 100   
+// VAR es mala palabra se usa "let o const"         
+console.log(1+2);
+console.log('hola mundo');
+// sumar dos valores y mostrar su resultado
+let op1 =prompt('Ingrese valor op1');
+let op2=prompt('Ingrese valor op2');
+let res = (+op1)+ parseInt(+op2)
+// muestro el resultado por consola
+console.log(res)
+//por ALERTA
+alert('resultado' + res); // muestra el resultado en cartel
